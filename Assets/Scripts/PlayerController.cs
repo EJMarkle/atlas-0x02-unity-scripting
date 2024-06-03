cd8994663e025264cb9f93391ef305a6a538f1b5
unknown <emarkle99@gmail.com>
2024-06-02 21:35:22 -0500

    coin collection and score debug msg

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody rb;
+    private int score = 0;
 
+    /// Preload get rigidbody component 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    /// X and Y axis movement
     void FixedUpdate()
     {
         float xMovement = Input.GetAxis("Horizontal");
@@ -19,5 +22,18 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(xMovement, 0f, yMovement) * speed;
         rb.velocity = movement;
+    }
+
+    /// On collision with coin, increment player score and deactivate object
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            score++;
+
+            Debug.Log("Score: " + score);
+
+            other.gameObject.SetActive(false);
+        }
     }
 }
